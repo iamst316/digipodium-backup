c105c3c2a3cef7dca639e757fe40f3955708d84b
Shubham Tandon <iamshubham316@gmail.com>
2022-09-24 16:42:15 +0530

    images

--- digipodium/django/homework/homework/settings.py
+++ digipodium/django/homework/homework/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATIC_URL = '/static/'
+STATIC_ROOT = [BASE_DIR / "static"]
+STATICFILES_DIR = [str(os.path.join(BASE_DIR, 'static')),]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
--- digipodium/django/homework/homework/urls.py
+++ digipodium/django/homework/homework/urls.py
@@ -17,8 +17,13 @@ from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='gridimg.html')),
+    
 ]
+static(settings.STATIC_URL,document_root=settings.STATIC_URL),
