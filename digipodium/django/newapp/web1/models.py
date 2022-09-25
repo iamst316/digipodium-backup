a486d511797a566090d5ef7e9df40e743365efd2
Shubham Tandon <iamshubham316@gmail.com>
2022-09-25 18:38:33 +0530

    db

--- digipodium/django/newapp/web1/models.py
+++ digipodium/django/newapp/web1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import *
 
 # Create your models here.
+
+class Movies(models.Model):
+    title= CharField(max_length=50)
+    year= IntegerField()
+    rating= FloatField()
+    def __str__(self) -> str:
+        return self.title
+class Show(models.Model):
+    title= CharField(max_length=60)
+    year= IntegerField()
+    rating= FloatField()
+    seasons= IntegerField()
+    created_at= DateTimeField(auto_now=True)
