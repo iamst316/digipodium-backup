76364bf5e3040efad752754d961e40e21e01c74e
Shubham Tandon <iamshubham316@gmail.com>
2022-09-28 18:42:27 +0530

    new models

--- digipodium/django/newapp/web1/admin.py
+++ digipodium/django/newapp/web1/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Movies, Show
+from .models import Movies, Show, Student, Report, Weather
 # Register your models here.
 admin.site.register(Movies)
 admin.site.register(Show)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    '''Admin View for Student'''
+
+    list_display = ('rollno','name','klass')
+    search_fields = ('name','klass')
+    
+    ordering = ('name',)
--- digipodium/django/newapp/web1/models.py
+++ digipodium/django/newapp/web1/models.py
@@ -1,3 +1,5 @@
+from ast import For
+from unicodedata import name
 from django.db import models
 from django.db.models import *
 
@@ -9,9 +11,40 @@ class Movies(models.Model):
     rating= FloatField()
     def __str__(self) -> str:
         return self.title
+
+
 class Show(models.Model):
     title= CharField(max_length=60)
     year= IntegerField()
     rating= FloatField()
     seasons= IntegerField()
     created_at= DateTimeField(auto_now=True)
+
+class Student(models.Model):
+    name= CharField(max_length=35)
+    klass= IntegerField(verbose_name='Class')
+    rollno= IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Report(models.Model):
+    english= IntegerField()
+    maths= IntegerField()
+    science=IntegerField()
+    hindi= IntegerField()
+    socsci=IntegerField()
+    student= ForeignKey('Student',on_delete=DO_NOTHING)
+
+    def __str__(self) -> str:
+        return f'Report of {self.student.name}'
+
+class Weather(models.Model):
+    temp= DecimalField(verbose_name="Temp(C)",max_digits=4, decimal_places=2)
+    wind_speed= DecimalField(max_digits=4, decimal_places=2)
+    humidity= DecimalField(max_digits=4, decimal_places=2)
+    date= DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.temp
